371a62a5eae470ce6edb7a7d8b3063b094f20a8f
Danial Afiq <danialafiq.dev@gmail.com>
2026-03-03 03:01:30 +0800

    updated Player Class: getName(), getGems(), getPurchasedCards(), getReservedCards(), getNobles, getPoints(), override toString()

--- src/splendor/entities/Player.java
+++ src/splendor/entities/Player.java
@@ -1,3 +1,68 @@
+package splendor.entities;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+
 public class Player {
+    private String name;
+    private EnumMap<GemColor, Integer> gems;
+    private List<Card> purchasedCards;
+    private List<Card> reservedCards;
+    private List<Noble> nobles;
+
+    public Player(String name) {
+        this.name = name;
+        this.gems = new EnumMap<>(GemColor.class);
+
+        for (GemColor color : GemColor.values()) {
+            this.gems.put(color, 0);
+        }
+        this.purchasedCards = new ArrayList<Card>();
+        this.reservedCards = new ArrayList<Card>();
+        this.nobles = new ArrayList<Noble>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public EnumMap<GemColor, Integer> getGems() {
+        return gems;
+    }
+
+    public List<Card> getPurchasedCards() {
+        return purchasedCards;
+    }
+
+    public List<Card> getReservedCards() {
+        return reservedCards;
+    }
+
+    public List<Noble> getNobles() {
+        return nobles;
+    }
+    
+    public int getPoints() {
+        int total = 0;
+        for (Card card : purchasedCards) {
+            total += card.getPoints();
+        }
+        for (Noble noble : nobles) {
+            total += noble.getPoints();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "\n name = " + name +
+                ",\n gems = " + gems +
+                ",\n purchasedCards = " + purchasedCards +
+                ",\n reservedCards = " + reservedCards +
+                ",\n nobles = " + nobles +
+                "\n}";
+    }
 
 }
