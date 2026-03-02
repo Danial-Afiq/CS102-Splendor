e5b4c5425aa04666abd082361de62ed7435255ef
Danial Afiq <danialafiq.dev@gmail.com>
2026-03-03 03:07:34 +0800

    added test code in main function for Player, Noble & GemBank creation

--- src/splendor/app/Main.java
+++ src/splendor/app/Main.java
@@ -1,11 +1,26 @@
 package splendor.app;
-
-import splendor.entities.GemColor;
-import splendor.entities.Tier;
+import java.util.EnumMap;
+import splendor.entities.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(GemColor.RUBY);
-        System.out.println(Tier.TWO);
+        //creates player object
+        Player p1 = new Player("Danial");
+        System.out.println(p1);
+        System.out.println();
+
+        //create one noble
+        EnumMap<GemColor, Integer> req = new EnumMap<>(GemColor.class);
+        req.put(GemColor.RUBY, 3);
+        req.put(GemColor.EMERALD, 3);
+        req.put(GemColor.ONYX, 3);
+        Noble n1 = new Noble("N1", 3, req);
+        System.out.println(n1);
+
+        System.out.println();
+
+        //create the gem bank
+        GemBank gemBank = new GemBank(3);
+        System.out.println(gemBank);
     }
 }
